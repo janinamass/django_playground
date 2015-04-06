705ee0adc46aa399c87c9b5c068022e3e440cd10
Gogs <janina.mass@gmail.com>
2015-04-06 19:55:08 +0200

    html output

--- resttutorial/snippets/serializers.py
+++ resttutorial/snippets/serializers.py
@@ -5,12 +5,14 @@ from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style','owner',)
+        fields = ('url','highlight', 'title', 'code', 'linenos', 'language', 'style','owner',)
 
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'username', 'snippets')
